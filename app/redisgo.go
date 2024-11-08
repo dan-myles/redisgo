bc87655447400b22b3d7a280844ee08464daa323
dan-myles <79137382+dan-myles@users.noreply.github.com>
2024-11-07 20:22:27 -0700

    feat: configurable port flag

--- app/redisgo.go
+++ app/redisgo.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 )
 
 var VALKEY = NewSys()
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	port := "6379"
+	if len(os.Args) >= 3 {
+		port = os.Args[2]
+	}
+
+	l, err := net.Listen("tcp", "0.0.0.0:"+port)
 	if err != nil {
 		panic(err)
 	}
